df83f372dd804dac1ee8a01a3b45651ec8997a3a
antony <antonydelacruzramos@gmail.com>
2024-08-16 00:13:35 -0600

    Config Ubuntu

--- app/app.py
+++ app/app.py
@@ -28,7 +28,7 @@ csrf=CSRFProtect()
 def get_db_connection():
     try:
         conn = psycopg2.connect(host="localhost",
-                                dbname="SPVBMA",
+                                dbname="spvbma",
                                 user="postgres",
                                 password="gojo"),
                                 
